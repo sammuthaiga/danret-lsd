50cb349d7a59a0f5bc7dd39b1c97dff18e036848
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-12-11 07:17:44 +0300

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+ 
+
+  return (
+    <h1 className="text-3xl text-green-400 font-bold underline">
+    Hello world! <span className="text-green-900 font-extrabold underline-offset-1">Sam the Dev.</span>
+  </h1>
+  )
+}
+
+export default App
